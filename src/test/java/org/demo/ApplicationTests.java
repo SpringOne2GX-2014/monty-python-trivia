f0457f3c0d5b2c017385337ab741cbd41d5b46b3
Ken Krueger <kenkrueger65@gmail.com>
2014-07-15 15:37:45 -0400

    latest

--- src/test/java/org/demo/ApplicationTests.java
+++ src/test/java/org/demo/ApplicationTests.java
@@ -1,18 +1,12 @@
 package org.demo;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
-@WebAppConfiguration
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(format = "pretty")
 public class ApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
 
 }
