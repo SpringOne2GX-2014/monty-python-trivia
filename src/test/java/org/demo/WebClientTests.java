188723807170775351156255925a63752bd1ba83
Rob Winch <rwinch@gopivotal.com>
2014-09-02 17:37:54 -0500

    Fix integration test

--- src/test/java/org/demo/WebClientTests.java
+++ src/test/java/org/demo/WebClientTests.java
@@ -1,25 +1,29 @@
 package org.demo;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
-import com.gargoylesoftware.htmlunit.CollectingAlertHandler;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@IntegrationTest
+@WebAppConfiguration
+@SpringApplicationConfiguration(classes = Config.class)
 public class WebClientTests {
 
 	//	This is a use of the raw HTMLUnit WebClient.  It requires a server running and a
 	//	URL that you can actually hit.  Good for debugging problems.
-	@Test
-	public void test() throws FailingHttpStatusCodeException, MalformedURLException, IOException {
+	@Test(expected = FailingHttpStatusCodeException.class)
+	public void test() throws Exception {
 	    WebClient webClient = new WebClient(BrowserVersion.CHROME);
         webClient.getPage("http://localhost:8080/mpt/dummy");
 	}
